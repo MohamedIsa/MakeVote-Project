43039701af2225c695fb535538414e37402236f6
MohamedIsa <gogetassj4ul4@gmail.com>
2024-12-02 22:47:15 +0300

    fixing session in home.php

--- src/home.php
+++ src/home.php
@@ -1,26 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+session_start();
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
-    <link rel="stylesheet" href="pstyle.css">
+if (isset($_SESSION['Active'])) {
 
+    if (isset($_GET['alert'])) {
+        $alert = $_GET['alert'];
+        if ($alert === "success") {
+            echo "<script>";
+            echo "alert('you created poll succesfully');";
+            echo "</script>";
+        }
+    }
 
-
-
-
-</head>
-
-<body>
-
+?>
 
 
 
-</body>
+    <!DOCTYPE html>
+    <html lang="en">
 
-</html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home Page</title>
+        <link rel="stylesheet" href="pstyle.css">
+
+
+
+
+
+    </head>
+
+    <body>
+
+
+
+
+
+        <?php
+
+        try {
+            require("headerLogin.php");
+            require("showQ.php");
+            require("footer.php");
+        } catch (PDOException $e) {
+            die("Error :" . $e);
+        }
+
+
+
+        ?>
+
+
+
+
+
+    </body>
+
+    </html>
 
 
 
@@ -28,7 +65,43 @@
 
 
 
+<?php
+} else {
 
-</body>
+?>
 
-</html>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home Page</title>
+    </head>
+
+    <body>
+
+        <?php require("header.php"); ?>
+        <hr><?php
+
+            require("showQnotlogin.php");
+            require("footer.php");
+
+            ?>
+
+
+
+
+
+    </body>
+
+    </html>
+
+
+<?php
+}
+
+
+
+
+?>
